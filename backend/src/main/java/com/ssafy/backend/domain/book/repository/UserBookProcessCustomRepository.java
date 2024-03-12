f5517dbc26ae999d5c77643e17ca2123280bb283
강대수 <sore503@naver.com>
2024-03-12 17:55:01 +0900

    ✨feat: #S10P21C101-258-1 책 클릭시 조회 로직 구현
    Former-commit-id: fc76926447688ae25e806caa1ed00336fe8ba346 [formerly 31a245ee04ac70cf386c00f51bfa64b73defcf42]
    Former-commit-id: 12bcf46bcae3552bea5ab8fc2dbb941180920e8b

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/UserBookProcessCustomRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/UserBookProcessCustomRepository.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:030555369a6b62f85ac518a466ee937b73957f8c4253ad936196d83af107be1c
-size 489
+package com.ssafy.backend.domain.book.repository;
+
+import com.ssafy.backend.domain.book.entity.BookPage;
+import com.ssafy.backend.domain.book.entity.BookPageSentence;
+import com.ssafy.backend.domain.education.entity.Education;
+
+import java.util.List;
+
+public interface UserBookProcessCustomRepository {
+
+    List<BookPage> findByBookId(Long bookId);
+    List<BookPageSentence> findByBookPageId(List<Long> bookId);
+    List<Education> findByBookSentenceId(List<Long> bookPageSentenceId);
+}
