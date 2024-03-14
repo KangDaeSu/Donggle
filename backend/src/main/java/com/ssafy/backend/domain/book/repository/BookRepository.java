08b8ac2208f388d799d9bc7244eca9a7fcf22619
강대수 <sore503@naver.com>
2024-03-14 13:32:58 +0900

    ✨feat: #S10P21C101-271 진행중인 책, 구매한 책 조회 구현
    Former-commit-id: 04d549fbaeb7553c6e61ae260082d5392b6d9acb [formerly 87b4ae8704bceb07e357b01e3c90ee97d0086e57]
    Former-commit-id: 16dc9cb742102c1753ba2d8c04d06b99b1e6eff5

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.backend.domain.book.repository;
-
-import com.ssafy.backend.domain.book.dto.BookInfoDto;
-import com.ssafy.backend.domain.book.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
