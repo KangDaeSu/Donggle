3d3eca4304ddd87e6709fadcf2304b608426df63
김민주 <ducco705@snu.ac.kr>
2024-03-18 17:44:43 +0900

    :construction: feat: #S10P22C101-177 줄거리 불러오기 완료, 평점 불러오기 구현 중
    Former-commit-id: 666fbc530b0e3acefa9484767f8783e04d90d5fb [formerly 9a01588514a99b657b49a776dabe85453f621d5a]
    Former-commit-id: a757b7f4ac7bc94363caf2b06b45df22571e355d

--- frontend/lib/domain/model/model_review.dart
+++ frontend/lib/domain/model/model_review.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77cc1e34b806c94186bcc880c38c6ca8c3b3db859a79fbaaf252f8cfcb502165
-size 289
+oid sha256:dee19a31fffa510d12e1c357653c3693ef1004626ddbbe3316ef26d8ee8d48eb
+size 567
