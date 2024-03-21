871f287a829cdb2cee60ce31fcd87277501e3be9
강대수 <sore503@naver.com>
2024-03-21 12:18:13 +0900

    ♻️refactor: #S10P21C101-323 코드 리펙토링
    Former-commit-id: e291716d59aca3fa33b58280b7bc7a1b3d73ba19 [formerly a1926518ea45dd86be7758ec080a3f84ad138142]
    Former-commit-id: 04a1437a91a7830d8166b581d30af820d4debce8

--- backend/src/main/java/com/ssafy/backend/domain/book/repository/book/BookCustomRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/repository/book/BookCustomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fd938077b8b87e551e94bac2062490a899f63f10693ab18cf716be38a5d7315
-size 211
+oid sha256:7a1d2882c3116ef22ef27c0c20e49a972ecb38092d1911b9c7517928c36eaec4
+size 249
