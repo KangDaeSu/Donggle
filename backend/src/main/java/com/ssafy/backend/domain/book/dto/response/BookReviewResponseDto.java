467f4c6d06327a442e507e9296eceb1c93d404f9
강대수 <sore503@naver.com>
2024-03-25 16:37:18 +0900

    ♻ refactor:#S10P21C101-339 코드리펙토링, 책 조회 수정
    Former-commit-id: 00cf198dc9d220abe82dca6a7842b52a6393af7f [formerly 89e6451f98bf5d5d0477138b6900c172b76f15c2]
    Former-commit-id: 03a94d7556c8bd597bf8221e3415e65c877c147d

--- backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/book/dto/response/BookReviewResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfcf3db8149ed0588e1528731d17b8c1711a767790ab20ad7dafd97d7e25e68f
-size 174
+oid sha256:8ae2fb67fb8bbfbf70e3877c7ebec90bb9b28e8630c65c67d2886a34df03e89b
+size 673
